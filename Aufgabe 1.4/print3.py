75787fb6a5d258263d04ac0651b498dc8ad171fc
Ege Tekin <et130@students.uni-freiburg.de>
2023-10-22 12:26:48 +0300

    update3

--- Aufgabe 1.4/print3.py
+++ Aufgabe 1.4/print3.py
@@ -4,5 +4,4 @@ y = 10**4047
 # Die Zahl y dient dazu, dass man die letzen 4047 Ziffern von x durch mod finden kann.
 s = 10**4043
 # Die Zahl s entfernt die überflüssigen Zahlen aus dem Ergebnis von x mod y.
-
 print((x % y) // s)
